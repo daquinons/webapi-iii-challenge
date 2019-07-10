372db60843f2793edda7386bc34e3bb5aff93616
David Quiñones <daquinons@users.noreply.github.com>
2019-07-10 15:00:35 +0200

    Add validatePost

--- users/userRouter.js
+++ users/userRouter.js
@@ -90,6 +90,15 @@ function validateUser(req, res, next) {
   }
 }
 
-function validatePost(req, res, next) {}
+function validatePost(req, res, next) {
+  const post = req.body;
+  if (!post) {
+    res.status(400).json({ message: "missing post data" });
+  } else if (!post.text) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
