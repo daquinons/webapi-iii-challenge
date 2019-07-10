dda1eecfbd4038f6fd90fc14320039c77e1903b3
David Quiñones <daquinons@users.noreply.github.com>
2019-07-10 14:42:18 +0200

    Add validateUserId, get and delete endpoints for user

--- users/userRouter.js
+++ users/userRouter.js
@@ -8,7 +8,7 @@ router.post('/', validateUser, async (req, res) => {
   const user = req.body;
   const createdUser = await Users.insert(user);
 
-  res.json(createdUser);
+  res.status(201).json(createdUser);
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -24,16 +24,23 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+  const user = req.user;
+  res.json(user);
 });
 
 router.get('/:id/posts', (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validateUserId, async (req, res) => {
+  try {
+    const user = req.user;
+    const userRemoved = await Users.remove(user.id);
+    res.json({...user, removed: userRemoved ? true : false});
+  } catch (error) {
+    res.status(500).json({ message: 'There was a problem deleting the user' });
+  }
 });
 
 router.put('/:id', (req, res) => {
@@ -42,8 +49,16 @@ router.put('/:id', (req, res) => {
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
+  const { id } = req.params;
+  const user = await Users.getById(id);
 
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(400).json({ message: "invalid user id" });
+  }
 };
 
 function validateUser(req, res, next) {
